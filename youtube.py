a23dc15b191aa8e8c4197e4f1f3d3a6c768627bb
yeonyii0 <masanlny0409@gmail.com>
2025-03-24 07:32:31 +0000

    밀리

--- youtube.py
+++ youtube.py
@@ -12,16 +12,24 @@ headers = {
 # 요청 보내기
 response = requests.get(url, headers=headers)
 
-# HTML 파싱
-soup = BeautifulSoup(response.text, 'html.parser')
+# 응답 상태 코드 확인
+print("응답 상태 코드:", response.status_code)
 
-# 급상승 동영상 정보 추출 (예: 제목, 링크)
-video_items = soup.find_all('ytd-video-renderer')
+if response.status_code == 200:
+    # HTML 파싱
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# 데이터 출력
-for video in video_items:
-    title = video.find('a', {'id': 'video-title'})
-    if title:
-        video_title = title.text
-        video_url = 'https://www.youtube.com' + title['href']
-        print(f"제목: {video_title}\nURL: {video_url}\n")
+    # 급상승 동영상 정보 추출 (예: 제목, 링크)
+    video_items = soup.find_all('ytd-video-renderer')
+
+    # 데이터 출력
+    for video in video_items:
+        title = video.find('a', {'id': 'video-title'})
+        if title:
+            video_title = title.text.strip()
+            video_url = 'https://www.youtube.com' + title['href']
+            print(f"제목: {video_title}\nURL: {video_url}\n")
+else:
+    print("유튜브 페이지를 가져오는 데 실패했습니다.")
+
+
